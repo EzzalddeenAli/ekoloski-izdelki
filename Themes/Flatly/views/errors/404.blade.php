63b7688ec88e6eefcb1294782bbd362c402739a4
Bojan Kovacec <bojan.kovacec@gmail.com>
2017-05-28 19:19:07 +0200

    Basic error pages.

---
+++ Themes/Flatly/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('title')
+    Not found | @parent
+@stop
+
+@section('content')
+    <div class="row">
+        <h1>404: page not found</h1>
+        <p>Looks like page was removed or not created yet.</p>
+    </div>
+@stop
---
+++ Themes/Flatly/views/errors/500.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('title')
+    Not found | @parent
+@stop
+
+@section('content')
+    <div class="row">
+        <h1>500: ups something went wrong</h1>
+        <p>Our developers are working on it.</p>
+    </div>
+@stop
