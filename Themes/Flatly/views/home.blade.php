a0f8bee6a9cc327abda90e14911423a10e158284
Bojan Kovacec <bojan.kovacec@gmail.com>
2017-06-11 17:54:11 +0200

    Paralax effect on home screen.

--- Themes/Flatly/views/home.blade.php
+++ Themes/Flatly/views/home.blade.php
@@ -9,104 +9,137 @@
 @stop
 
 @section('content')
-    <div class="container">
-    {{--
-    <div class="row">
-        <h1>{{ $page->title }}</h1>
-        {!! $page->body !!}
-    </div>
-    --}}
+    <style>
+        .parallax {
+            /* The image used */
+            background-image: url("/themes/flatly/css/home2.JPG");
 
-        <div id="app">
-            <input type="hidden" id="fingerprint" v-model="fingerprint">
+            /* Set a specific height */
+            height: 600px;
 
-            {{--
-            <input type="checkbox" v-model="checked">
-            <p>@{{ checked }}</p>
-            --}}
+            /* Create the parallax scrolling effect */
+            background-attachment: fixed;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    </style>
 
-            {{--
-            <input type="checkbox" value="Mike" v-model="checkedNames">
-            <label >Mike</label>
-            <input type="checkbox" value="Mike1" v-model="checkedNames">
-            <label >Mike1</label>
-            <input type="checkbox" value="Mike2" v-model="checkedNames">
-            <label >Mike2</label>
-            <p>@{{checkedNames}}</p>
-            --}}
-
-            {{--
-            <input type="radio" value="Mike" v-model="radioPicks">
-            <label >Mike</label>
-            <input type="radio" value="Mike1" v-model="radioPicks">
-            <label >Mike1</label>
-            <input type="radio" value="Mike2" v-model="radioPicks">
-            <label >Mike2</label>
-            <p>@{{radioPicks}}</p>
-            --}}
-
-            {{--
-            <div class="row">
-                <div class="col-md-4 text-center" style="padding-top: 2em;">
-                    <i class="fa fa-share-alt" aria-hidden="true" style="font-size: 7em;"></i>
-                </div>
-                <div class="col-md-8">
-                    <h2> Od kmeta do zlice. </h2>
-                    <p>
-                        S pomocjo novih tehnologij, lahko omogocimo a je pot hrane do potrosnika vedno krajsa in cenejsa.
-
-                        <ul>
-                            <li>informacije - uporabniska iskusnja = enostavnost uporabe (profil, sms, mail, push notification, apps, ..)</li>
-                            <li>elektricni avtomobili, drone, solarni hladilnik</li>
-                            <li>prosti trg - vsak lahko uporablja storitev tako za pridobivanje informacij in izdelkov kot tudi objavljanje in prodajo izdelkov</li>
-                        </ul>
+    <!-- Container element
+    <div class="parallax"></div>
+-->
 
 
-                    </p>
-                    <p> Ljudje se vse bol zavedajo da je pomembno kar dajemo v usta. </p>
-                </div>
-            </div>
 
-            <div class="row">
+    <div class="container-fluid">
 
-                <div class="col-md-8">
-                    <strong>Ekolosko</strong>
-
-                </div>
-
-                <div class="col-md-4 text-center" style="padding-top: 2em;">
-                    EKO
-                </div>
-            </div>
-            --}}
-
-
-            {{--
-            <select multiple v-model="selectedOpt">
-                <option v-for="option in options" v-bind:value="option.value">
-                    @{{option.text}}
-                </option>
-            </select>
-            @{{selectedOpt}}
-            --}}
-
-
-            {{--
-            <button v-on:click="greet">Greet</button>
-            --}}
-
-
-            {{--
-            <div v-for="(annt, index) in anns">
-                <button v-on:click="edit(index)">edit</button>
-                <p>state: @{{ annt.state }}</p>
-            </div>
-            --}}
-
-            @include('newsletter.partials.registration')
-
+        <div class="row parallax">
         </div>
+
+        {{--
+        <div class="row">
+            <h1>{{ $page->title }}</h1>
+            {!! $page->body !!}
+        </div>
+        --}}
+
     </div>
+
+   <div class="container">
+       <div class="row">
+           <div id="app">
+               <input type="hidden" id="fingerprint" v-model="fingerprint">
+
+               {{--
+               <input type="checkbox" v-model="checked">
+               <p>@{{ checked }}</p>
+               --}}
+
+               {{--
+               <input type="checkbox" value="Mike" v-model="checkedNames">
+               <label >Mike</label>
+               <input type="checkbox" value="Mike1" v-model="checkedNames">
+               <label >Mike1</label>
+               <input type="checkbox" value="Mike2" v-model="checkedNames">
+               <label >Mike2</label>
+               <p>@{{checkedNames}}</p>
+               --}}
+
+               {{--
+               <input type="radio" value="Mike" v-model="radioPicks">
+               <label >Mike</label>
+               <input type="radio" value="Mike1" v-model="radioPicks">
+               <label >Mike1</label>
+               <input type="radio" value="Mike2" v-model="radioPicks">
+               <label >Mike2</label>
+               <p>@{{radioPicks}}</p>
+               --}}
+
+               {{--
+               <div class="row">
+                   <div class="col-md-4 text-center" style="padding-top: 2em;">
+                       <i class="fa fa-share-alt" aria-hidden="true" style="font-size: 7em;"></i>
+                   </div>
+                   <div class="col-md-8">
+                       <h2> Od kmeta do zlice. </h2>
+                       <p>
+                           S pomocjo novih tehnologij, lahko omogocimo a je pot hrane do potrosnika vedno krajsa in cenejsa.
+
+                           <ul>
+                               <li>informacije - uporabniska iskusnja = enostavnost uporabe (profil, sms, mail, push notification, apps, ..)</li>
+                               <li>elektricni avtomobili, drone, solarni hladilnik</li>
+                               <li>prosti trg - vsak lahko uporablja storitev tako za pridobivanje informacij in izdelkov kot tudi objavljanje in prodajo izdelkov</li>
+                           </ul>
+
+
+                       </p>
+                       <p> Ljudje se vse bol zavedajo da je pomembno kar dajemo v usta. </p>
+                   </div>
+               </div>
+
+               <div class="row">
+
+                   <div class="col-md-8">
+                       <strong>Ekolosko</strong>
+
+                   </div>
+
+                   <div class="col-md-4 text-center" style="padding-top: 2em;">
+                       EKO
+                   </div>
+               </div>
+               --}}
+
+
+               {{--
+               <select multiple v-model="selectedOpt">
+                   <option v-for="option in options" v-bind:value="option.value">
+                       @{{option.text}}
+                   </option>
+               </select>
+               @{{selectedOpt}}
+               --}}
+
+
+               {{--
+               <button v-on:click="greet">Greet</button>
+               --}}
+
+
+               {{--
+               <div v-for="(annt, index) in anns">
+                   <button v-on:click="edit(index)">edit</button>
+                   <p>state: @{{ annt.state }}</p>
+               </div>
+               --}}
+
+               @include('newsletter.partials.registration')
+
+           </div>
+       </div>
+   </div>
+
+
 @stop
 
 
