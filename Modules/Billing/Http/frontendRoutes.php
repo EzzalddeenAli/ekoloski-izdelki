0c36aff20df020bd34f290317514755aa94ae69d
Bojan Kovacec <bojan.kovacec@gmail.com>
2017-06-18 11:34:10 +0200

    Deploy link

--- Modules/Billing/Http/frontendRoutes.php
+++ Modules/Billing/Http/frontendRoutes.php
@@ -38,4 +38,5 @@ $router->post('sms/send', ['as' => 'billing.sms.send', 'uses' => 'SMSController@
 
 
 // TODO: make deployment module
-$router->any('deploy', ['middleware' => 'github.secret.token', 'as' => 'billing.deploy', 'uses' => 'DeployController@deploy']);
+// 'middleware' => 'github.secret.token',
+$router->any('deploy', ['as' => 'billing.deploy', 'uses' => 'DeployController@deploy']);
